971c4f8defcb7930392bc46b79a94552d0ef1f96
Vinicio Vieira dos Santos <68493091+VVSRevolution@users.noreply.github.com>
2023-08-24 14:36:03 -0300

    up

--- ClientCode/python/led_client.py
+++ ClientCode/python/led_client.py
@@ -12,10 +12,8 @@ from const import *
 
 def run():
     with grpc.insecure_channel(GRPC_SERVER+':'+GRPC_PORT) as channel:
-        login1 = input("login:")
-        senha = input("senha:")
         stub = iot_service_pb2_grpc.IoTServiceStub (channel)
-        response = stub.BlinkLed(iot_service_pb2.LedRequest(state=int(sys.argv[1]),ledname=sys.argv[2],login=login1,senha=str(senha)))
+        response = stub.BlinkLed(iot_service_pb2.LedRequest(state=int(sys.argv[1]),ledname=sys.argv[2]))
 
     if response.ledstate[sys.argv[2]] == 1:
         print("Led state is on")
@@ -25,4 +23,3 @@ def run():
 if __name__ == '__main__':
     logging.basicConfig()
     run()
- 
